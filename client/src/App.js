a868204d5cf2336c93c5215db1034dd4ef231595
Eduarda Leal <edudardaleal753@gmail.com>
2025-03-20 20:16:10 -0300

    Baixa o react e o tailwind

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-purple-600 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-300">Testando Tailwind 4.0!</h1>
+    </div>
+  );
+}
+
+export default App;
