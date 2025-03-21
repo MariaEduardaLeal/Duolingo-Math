11ec5a7145220ef5e2500b6842a662f5026744a1
Eduarda Leal <edudardaleal753@gmail.com>
2025-03-20 21:06:07 -0300

    Muda para backend em php

---
+++ src/config.php
@@ -0,0 +1,13 @@
+<?php
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+$host = $env['DB_HOST'];
+$dbname = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
