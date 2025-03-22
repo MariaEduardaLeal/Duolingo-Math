f248d88f03c314712a52e813f704d7fd108aa09c
Eduarda Leal <edudardaleal753@gmail.com>
2025-03-21 22:40:58 -0300

    Ajeita a conexão de config.php

--- public_html/src/config.php
+++ public_html/src/config.php
@@ -15,7 +15,7 @@ $capsule->addConnection([
     'host'      => $_ENV['DB_HOST'],
     'database'  => $_ENV['DB_DATABASE'],
     'username'  => $_ENV['DB_USERNAME'],
-    'password'  => $_ENV['DB_PORT'],
+    'password'  => $_ENV['DB_PASSWORD'],
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
