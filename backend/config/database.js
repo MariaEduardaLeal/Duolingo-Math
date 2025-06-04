38b24c1b38d0419a76d4bf226de1155c7eb66806
Maria-Eduarda-Leal <71770176+MariaEduardaLeal@users.noreply.github.com>
2025-06-04 12:21:33 -0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,8 +18,8 @@ const sequelize = new Sequelize({
   logging: false,
   dialectOptions: {
     ssl: {
-      require: false, // Exige SSL
-      rejectUnauthorized: false // Garante que o certificado seja validado
+      require: true, // Exige SSL
+      rejectUnauthorized: true // Garante que o certificado seja validado
     }
   }
 });
